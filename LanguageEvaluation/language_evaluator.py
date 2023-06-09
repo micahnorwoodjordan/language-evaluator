74e573ef882d502e32de71523090485d06cfaa8e
Micah Norwood <micahnorwoodjordan@gmail.com>
2023-06-09 11:59:38 -0700

    Revert "configure processor for punctuation, proper noun, and <fix> recognition"
    This reverts commit 5fdc6ebd2efb32483e59261543260f7bf24b5cb2.

--- LanguageEvaluation/language_evaluator.py
+++ LanguageEvaluation/language_evaluator.py
@@ -1,6 +1,5 @@
-import enum
-
-from spacy import load, util, tokenizer, matcher
+import spacy
+from enum import Enum
 
 
 class UnsupportedLanguageException(Exception):
@@ -11,7 +10,7 @@ class LanguageEvaluationException(Exception):
     pass
 
 
-class SupportedLanguages(enum.Enum):
+class SupportedLanguages(Enum):
     english = 1
     spanish = 2
 
@@ -22,15 +21,7 @@ SPACY_LANGUAGE_MODEL_MAP = {
     SupportedLanguages.spanish: 'es_core_web_md',
 }
 
-EXTRA_INFIXES = {
-    'default': []  # only populate with regexes
-}
-
-MATCHING_PATTERNS = {
-    'PROPER_NOUNS': {
-        'FULL_NAME': [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
-    },
-}
+EXTRA_INFIXES = []  # only populate with regexes
 
 
 class LanguageEvaluator:
@@ -38,56 +29,22 @@ class LanguageEvaluator:
     evaluate the complexity of a series of strings entered by a user
     TODO: handle mispelled words
     TODO: handle conjunctions
-
-    TODO: consider analyzing token frequency within a user's Journal
-    TODO: consider part of speech (POS) tagging
-
-    reference: https://realpython.com/natural-language-processing-spacy-python
     """
-    def _configure_processor(self, language, **kwargs):
-        processor = load(SPACY_LANGUAGE_MODEL_MAP[language])  # natural language processor
+    def __init__(self, language):
+        self.NLP = spacy.load(SPACY_LANGUAGE_MODEL_MAP[language])  # natural language processor
+        prefix_re = spacy.util.compile_prefix_regex(self.NLP.Defaults.prefixes)
+        suffix_re = spacy.util.compile_suffix_regex(self.NLP.Defaults.suffixes)
+        infix_re = spacy.util.compile_infix_regex(list(self.NLP.Defaults.infixes) + EXTRA_INFIXES)
 
-        # tokenizer
         # NOTE: this is probably the only tokenizer use case, but new tokenizers can be constructed and swapped in/out
         # preceeding and proceeding punctuation, such as opening and closing parenthesis, need to be accounted for
-        prefix_re = util.compile_prefix_regex(processor.Defaults.prefixes)
-        suffix_re = util.compile_suffix_regex(processor.Defaults.suffixes)
-        infix_re = util.compile_infix_regex(list(processor.Defaults.infixes) + EXTRA_INFIXES['default'])
-        processor.tokenizer = tokenizer.Tokenizer(
-            processor.vocab,
+        self.NLP.tokenizer = spacy.tokenizer.Tokenizer(
+            self.NLP.vocab,
             prefix_search=prefix_re.search,
             suffix_search=suffix_re.search,
             infix_finditer=infix_re.finditer,
         )
 
-        # matcher
-        # parse proper nouns such as names
-        _matcher = matcher.Matcher(processor.vocab)  # avoid symbol clash
-        key = 'FULL_NAME'
-        pattern = MATCHING_PATTERNS['PROPER_NOUNS'][key]
-        _matcher.add(key, [pattern])
-        self.matcher = _matcher
-        self.NLP = processor
-
-    def __init__(self, language):
-        self._configure_processor(language)
-
     def tokenize_entry(self, entry):
-        # witness the spaCy magic happen. the instance's processer, tokenizer, and matcher are already configured at this point
-        def parse_matches(matches):
-            for _, start, end in matches:
-                span = doc[start:end]
-                yield span.text
-
-        tokens = set()
-        doc = self.NLP(entry)
-        matches = [
-            token for match in parse_matches(self.matcher(doc)) for token in match.split(' ')
-        ]  # TODO: it probably won't BUT, evaluate whether this can backfire on new pattern configurations
-
-        for token in doc:
-            if not token.is_punct and token.is_alpha:
-                token = token.lemma_  # only tokenize the base form of each token (do / doing / did / does ---> do)
-                if token not in tokens and token not in matches:  # don't count proper nouns as words
-                    tokens.add(token)
+        tokens = [str(token).strip() for token in self.NLP(entry) if not token.is_punct and token.is_alpha]
         return tokens
